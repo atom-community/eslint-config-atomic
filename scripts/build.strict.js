f28d7a419496efde34d03c7cefdd94a1321f2e8c
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-03 00:46:50 -0600

    fix: add script to generate the strict versions

---
+++ scripts/build.strict.js
@@ -0,0 +1,22 @@
+// script to generate the strict versions (without only-warn)
+
+const { readFile, writeFile } = require("fs").promises
+const { join, dirname, basename } = require("path")
+
+const fileMap = {
+  "index.js": "strict.js",
+  "react.js": "strict-react.js"
+}
+
+;(async function main(){
+  const root = dirname(__dirname)
+  const files = ["index.js", "react.js"]
+
+  const filesPaths = files.map((file) => join(root, file))
+
+  filesPaths.forEach(async (filePath) => {
+    const text = (await readFile(filePath)).toString()
+    const newText = text.replace(/"only-warn"/g, "")
+    await writeFile(join(root, fileMap[basename(filePath)]), newText)
+  })
+})()
