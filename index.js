22021fac316703a2b1af5c5e289833600b338132
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-06 17:17:30 -0600

    fix: add import to coffee

--- index.js
+++ index.js
@@ -83,7 +83,12 @@ module.exports = {
       files: ["**/*.coffee", "**/*.cson"],
       parser: "eslint-plugin-coffee",
       plugins: ["coffee", "node", "only-warn"],
-      extends: ["plugin:coffee/eslint-recommended", "plugin:optimize-regex/all", "plugin:coffee/prettier"],
+      extends: [
+        "plugin:coffee/eslint-recommended",
+        "plugin:optimize-regex/all",
+        "plugin:coffee/import",
+        "plugin:coffee/prettier",
+      ],
       rules: {
         ...pluginNodeRules,
       },
