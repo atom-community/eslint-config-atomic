128d8c3a8144684fab0992cd8b37a84ad5fa0dfd
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-05 00:35:18 -0600

    feat: add eslint rules for variables

--- src/official-eslint-rules.js
+++ src/official-eslint-rules.js
@@ -5,4 +5,12 @@
 exports.eslintRulesExtra = {
   // https://eslint.org/docs/rules/#strict-mode
   strict: "error", // require or disallow strict mode directives
+
+  // Source: https://eslint.org/docs/rules/#variables
+  // "init-declarations": "error", //require or disallow initialization in variable declarations
+  "no-label-var": "error", //disallow labels that share a name with a variable
+  // "no-restricted-globals": "error", //disallow specified global variables
+  "no-shadow": ["error", { builtinGlobals: true, hoist: "all", allow: [] }], //disallow variable declarations from shadowing variables declared in the outer scope
+  // "no-use-before-define": "error", // disallow the use of variables before they are defined
+
 }
