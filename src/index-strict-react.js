41c0105cb69259bf20324f4d6190df312f97e7ee
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-04 23:31:33 -0600

    chore: use a runtime script to build strict configs

---
+++ src/index-strict-react.js
@@ -0,0 +1,16 @@
+const nonStrictConfig = require("./index-react")
+
+// remove only-warn from javascript
+if (nonStrictConfig.plugins[nonStrictConfig.plugins.length - 1] === "only-warn") {
+  nonStrictConfig.plugins.pop()
+}
+
+// remove only-warn from overrides
+nonStrictConfig.overrides.forEach(overrides => {
+  if (overrides.plugins[overrides.plugins.length - 1] === "only-warn") {
+    overrides.plugins.pop()
+  }
+})
+
+
+module.exports = nonStrictConfig
---
+++ src/index-strict.js
@@ -0,0 +1,18 @@
+const nonStrictConfig = require("./index")
+
+
+// remove only-warn from javascript
+if (nonStrictConfig.plugins[nonStrictConfig.plugins.length - 1] === "only-warn") {
+  nonStrictConfig.plugins.pop()
+}
+
+// remove only-warn from overrides
+nonStrictConfig.overrides.forEach(overrides => {
+  if (overrides.plugins[overrides.plugins.length - 1] === "only-warn") {
+    overrides.plugins.pop()
+  }
+})
+
+console.log(nonStrictConfig)
+
+module.exports = nonStrictConfig
