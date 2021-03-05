b01b5411e6443a3d3bf4cfc81108bc39510f3da9
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-05 01:21:08 -0600

    feat: add eslint rules for best-practices

--- src/official-eslint-rules.js
+++ src/official-eslint-rules.js
@@ -3,6 +3,73 @@
  * The rules that might be useful in future are commented.
  */
 exports.eslintRulesExtra = {
+  // Source https://eslint.org/docs/rules/#best-practices
+  // "accessor-pairs": "error", // enforce getter and setter pairs in objects and classes
+  "array-callback-return": "error", // enforce `return` statements in callbacks of array methods
+  "block-scoped-var": "error", // enforce the use of variables within the scope they are defined
+  "class-methods-use-this": "error", // enforce that class methods utilize `this`
+  // complexity: "error", // enforce a maximum cyclomatic complexity allowed in a program
+  // "consistent-return": "error", // require `return` statements to either always or never specify values
+  curly: "error", // enforce consistent brace style for all control statements
+  "default-case": "error", // require `default` cases in `switch` statements
+  "default-case-last": "error", // enforce default clauses in switch statements to be last
+  "default-param-last": "error", // enforce default parameters to be last
+  "dot-notation": "error", // enforce dot notation whenever possible
+  eqeqeq: "error", // require the use of `===` and `!==`
+  "grouped-accessor-pairs": "error", // require grouped accessor pairs in object literals and classes
+  // "guard-for-in": "error", // require `for-in` loops to include an `if` statement
+  // "no-alert": "error", // disallow the use of `alert`, `confirm`, and `prompt`
+  "no-caller": "error", // disallow the use of `arguments.caller` or `arguments.callee`
+  "no-constructor-return": "error", // disallow returning value from constructor
+  "no-div-regex": "error", // disallow division operators explicitly at the beginning of regular expressions
+  "no-empty-function": "error", // disallow empty functions
+  "no-eq-null": "error", // disallow `null` comparisons without type-checking operators
+  "no-eval": "error", // disallow the use of `eval()`
+  "no-extend-native": "error", // disallow extending native types
+  "no-extra-bind": "error", // disallow unnecessary calls to `.bind()`
+  "no-extra-label": "error", // disallow unnecessary labels
+  "no-floating-decimal": "error", // disallow leading or trailing decimal points in numeric literals
+  "no-implicit-coercion": "error", // disallow shorthand type conversions
+  "no-implicit-globals": "error", // disallow declarations in the global scope // check
+  "no-implied-eval": "error", // disallow the use of `eval()`-like methods
+  "no-invalid-this": "error", // disallow `this` keywords outside of classes or class-like objects
+  "no-iterator": "error", // disallow the use of the `__iterator__` property
+  "no-labels": "error", // disallow labeled statements
+  "no-lone-blocks": "error", // disallow unnecessary nested blocks
+  "no-loop-func": "error", // disallow function declarations that contain unsafe references inside loop statements
+  // "no-magic-numbers": "error", // disallow magic numbers
+  "no-multi-str": "error", // disallow multiline strings
+  "no-new": "error", // disallow `new` operators outside of assignments or comparisons
+  "no-new-func": "error", // disallow `new` operators with the `Function` object
+  "no-new-wrappers": "error", // disallow `new` operators with the `String`, `Number`, and `Boolean` objects
+  // "no-nonoctal-decimal-escape": "error", // disallow `\8` and `\9` escape sequences in string literals
+  "no-octal-escape": "error", // disallow octal escape sequences in string literals
+  "no-param-reassign": ["error", { props: false }], // disallow reassigning `function` parameters
+  "no-proto": "error", // disallow the use of the `__proto__` property
+  // "no-restricted-properties": "error", // disallow certain properties on certain objects
+  "no-return-assign": "error", // disallow assignment operators in `return` statements
+  "no-return-await": "error", // disallow unnecessary `return await`
+  "no-script-url": "error", // disallow `javascript:` urls
+  "no-self-compare": "error", // disallow comparisons where both sides are exactly the same
+  "no-sequences": "error", // disallow comma operators
+  "no-throw-literal": "error", // disallow throwing literals as exceptions
+  // "no-unmodified-loop-condition": "error", // disallow unmodified loop conditions
+  "no-unused-expressions": "error", // disallow unused expressions
+  "no-useless-call": "error", // disallow unnecessary calls to `.call()` and `.apply()`
+  "no-useless-concat": "error", // disallow unnecessary concatenation of literals or template literals
+  // "no-useless-return": "error", // disallow redundant return statements
+  // "no-void": "error", // disallow `void` operators
+  // "no-warning-comments": "error", // disallow specified warning terms in comments
+  // "prefer-named-capture-group": "error", // enforce using named capture group in regular expression
+  "prefer-promise-reject-errors": ["error", { allowEmptyReject: true }], // require using Error objects as Promise rejection reasons
+  "prefer-regex-literals": "error", // disallow use of the `RegExp` constructor in favor of regular expression literals
+  radix: "error", // enforce the consistent use of the radix argument when using `parseInt()`
+  "require-await": "error", // disallow async functions which have no `await` expression  // check
+  // "require-unicode-regexp": "error", // enforce the use of `u` flag on RegExp
+  // "vars-on-top": "error", // require `var` declarations be placed at the top of their containing scope
+  // "wrap-iife": "error", // require parentheses around immediate `function` invocations
+  // yoda: "error", // require or disallow "Yoda" conditions
+
   // https://eslint.org/docs/rules/#strict-mode
   strict: "error", // require or disallow strict mode directives
 
