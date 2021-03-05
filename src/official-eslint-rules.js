01ecb554027478448c0ce684fe7b0db1ce1f6e68
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-05 00:35:02 -0600

    feat: add strict mode rule

---
+++ src/official-eslint-rules.js
@@ -0,0 +1,8 @@
+/** The official Eslint rules that are constructive and not included in recommended.
+ * The formatting/stylistic rules are not among these.
+ * The rules that might be useful in future are commented.
+ */
+exports.eslintRulesExtra = {
+  // https://eslint.org/docs/rules/#strict-mode
+  strict: "error", // require or disallow strict mode directives
+}
