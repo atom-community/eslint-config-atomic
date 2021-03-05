6e9ba7551577960a1bcbc5421ac1984fd79e24a2
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-04 23:15:53 -0600

    chore: add plugin-node-rules

---
+++ src/plugin-node-rules.js
@@ -0,0 +1,8 @@
+exports.pluginNodeRules = {
+  "node/no-exports-assign": "error",
+  "node/process-exit-as-throw": "error",
+  "node/shebang": "error",
+  "node/no-deprecated-api": "error",
+  "node/prefer-promises/dns": "error",
+  "node/prefer-promises/fs": "error",
+}
