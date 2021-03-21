4330074783ba8e270d079c3d0bf92834fc07515f
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-21 00:51:48 -0500

    fix: disable no-unused-expressions in cson files

--- src/cson.js
+++ src/cson.js
@@ -3,4 +3,8 @@ const { coffeeConfig } = require("./coffeescript")
 exports.csonConfig = {
   ...coffeeConfig,
   files: ["**/*.cson"],
+  rules: {
+    // cson expressions are exported by default
+    "no-unused-expressions": "off",
+  },
 }
